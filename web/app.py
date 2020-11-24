058f090c952f0680d186b71d1857d9b7584a7506
James Addison <james@reciperadar.com>
2020-11-24 16:20:07 +0000

    Add product combination landing page generation

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
+import requests
 
 app = Flask(__name__)
 app.config.update({'FREEZER_DESTINATION': '../public'})
@@ -16,9 +17,59 @@ def product_view(product_id):
     return render_template('product.html', product=product)
 
 
+def products_from_path(path):
+    products = path.split('/')
+    products = [{'singular': product} for product in products]
+    return products
+
+
+@app.route('/product-combinations/+/<path:include>/')
+def product_combination_view(include):
+    include = products_from_path(include)
+    return render_template(
+        'combination.html',
+        include=include
+    )
+
+
+@app.route('/product-combinations/+/<path:include>/-/<path:exclude>/')
+def product_combination_with_exclusions_view(include, exclude):
+    include = products_from_path(include)
+    exclude = products_from_path(exclude)
+    return render_template(
+        'combination.html',
+        include=include,
+        exclude=exclude
+    )
+
+
+def spider_combinations(include=None, exclude=None):
+    response = requests.get(
+        url='https://www.reciperadar.com/api/recipes/explore',
+        params={
+            'include[]': include,
+            'exclude[]': exclude,
+        }
+    )
+    products = response.json()['facets']['products']
+    include = [product['key'] for product in products[:3]]
+    exclude = [product['key'] for product in products[-1:]]
+    yield include, exclude
+
+
 @freezer.register_generator
 def product_url_generator():
     yield product_view.__name__, {'product_id': 'example_id'}
+
+
+@freezer.register_generator
+def product_combination_generator():
+    for include, exclude in spider_combinations():
+        params = {'include': '/'.join(include)}
+        yield product_combination_view.__name__, params
+        if exclude:
+            params = {**params, **{'exclude': '/'.join(exclude)}}
+            yield product_combination_with_exclusions_view.__name__, params
 
 
 if __name__ == '__main__':
