d5118175e8edb21e21599734771576c33013afa4
James Addison <james@reciperadar.com>
2020-12-10 12:39:45 +0000

    Refactor: use just-in-time rendering during content spidering

--- web/app.py
+++ web/app.py
@@ -1,11 +1,16 @@
+import json
+from pathlib import Path
 from urllib3.util import Retry
 
-from flask import Flask, render_template
+from flask import Flask, jsonify
 from flask_frozen import Freezer
 import requests
 
+
+content_directory = Path(__file__).parent.parent.joinpath('public').resolve()
+
 app = Flask(__name__)
-app.config.update({'FREEZER_DESTINATION': '../public'})
+app.config.update({'FREEZER_DESTINATION': content_directory})
 
 freezer = Freezer(app)
 requests_session = requests.Session()
@@ -20,50 +25,43 @@ requests_adapter = requests.adapters.HTTPAdapter(max_retries=Retry(
 ))
 requests_session.mount('https', requests_adapter)
 
+url_queue = []
+
 
 def products_from_path(path):
-    products = path.split('/')
-    products = [{'singular': product} for product in products]
-    return products
+    return path.split('/')
 
 
-@app.route('/product-combinations/+/<path:include>/')
-def product_combination_view(include):
-    include = products_from_path(include)
-    return render_template(
-        'combination.html',
-        include=include
-    )
+def product_combination_url(include, exclude):
+    url = '/product-combinations'
+    if include:
+        url += '/+/'
+        url += '/'.join(include)
+    if exclude:
+        url += '/-/'
+        url += '/'.join(exclude)
+    url += '/data.json'
+    return url
 
 
-@app.route('/product-combinations/+/<path:include>/-/<path:exclude>/')
-def product_combination_with_exclusions_view(include, exclude):
-    include = products_from_path(include)
-    exclude = products_from_path(exclude)
-    return render_template(
-        'combination.html',
-        include=include,
-        exclude=exclude
-    )
-
-
-def spider_combinations(include=None, exclude=None):
+def render_content(include=None, exclude=None):
     include = include or []
     exclude = exclude or []
-    depth = len(set(include + exclude))
-
-    if depth > 3:
-        return
-
     print(f'* Requesting include={include} exclude={exclude} ... ', end='')
 
-    ingredients = include + [f'-{product}' for product in exclude]
+    params = {
+        'ingredients[]': include + [f'-{product}' for product in exclude]
+    }
     response = requests_session.get(
         url='https://www.reciperadar.com/api/recipes/explore',
-        params={'ingredients[]': ingredients}
+        params=params
     ).json()
 
     print(' done')
+
+    products = []
+    product_combinations = []
+    recipes = []
 
     total = response['total']
     choices = [product for product in response['facets']['products']]
@@ -71,22 +69,69 @@ def spider_combinations(include=None, exclude=None):
         product, count = choice['key'], choice['count']
         if count < 10:
             continue
-        yield from spider_combinations(include + [product], exclude)
+        url = product_combination_url(include + [product], exclude)
+        product_combinations.append(url)
         if include and count > total * 0.3:
-            yield from spider_combinations(include, exclude + [product])
-    if len(include) >= 2:
-        yield include, exclude
+            url = product_combination_url(include, exclude + [product])
+            product_combinations.append(url)
+
+    return {
+        'products': products,
+        'product-combinations': product_combinations,
+        'recipes': recipes,
+    }
+
+
+@app.route('/product-combinations/data.json')
+def product_combinations_root():
+    content = render_content()
+    # set noindex response header
+    return jsonify(content)
+
+
+@app.route('/product-combinations/+/<path:include>/data.json')
+def product_combination_view(include):
+    include = products_from_path(include)
+    content = render_content(include)
+    if len(include) < 2:
+        # set noindex response header
+        pass
+    return jsonify(content)
+
+
+@app.route('/product-combinations/+/<path:include>/-/<path:exclude>/data.json')
+def product_combination_with_exclusions_view(include, exclude):
+    include = products_from_path(include)
+    exclude = products_from_path(exclude)
+    content = render_content(include, exclude)
+    if len(include) < 2:
+        # set noindex response header
+        pass
+    return jsonify(content)
 
 
 @freezer.register_generator
 def product_combination_generator():
-    for include, exclude in spider_combinations():
-        params = {'include': '/'.join(include)}
-        yield product_combination_view.__name__, params
-        if exclude:
-            params = {**params, **{'exclude': '/'.join(exclude)}}
-            yield product_combination_with_exclusions_view.__name__, params
+    yield product_combinations_root.__name__, {}
+    yield from url_queue
+
+
+def extract_links(content):
+    if isinstance(content, dict):
+        for key, value in content.items():
+            yield from extract_links(value)
+        return
+    if isinstance(content, list):
+        for item in content:
+            yield from extract_links(item)
+        return
+    yield content
 
 
 if __name__ == '__main__':
-    freezer.freeze()
+    for url, path in freezer.freeze_yield():
+        path = Path(content_directory, path)
+        with open(path) as f:
+            content = f.read()
+            content = json.loads(content)
+            url_queue += extract_links(content)
