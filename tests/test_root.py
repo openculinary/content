983f7f415b88dfeb3e554463f578654b625d012f
James Addison <james@reciperadar.com>
2020-12-10 13:59:28 +0000

    Update test coverage

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,4 +1,24 @@
+import json
+import responses
+
+
+@responses.activate
 def test_request(client):
-    response = client.get('/products/example_id', follow_redirects=True)
-    assert response.mimetype == 'text/html'
-    assert response.data.decode() == '<div>example</div>'
+    responses.add(
+        method=responses.GET,
+        url='https://www.reciperadar.com/api/recipes/explore',
+        body=json.dumps({
+            'facets': {
+                'products': [
+                    {'key': 'few_results', 'count': 5},
+                    {'key': 'many_results', 'count': 50},
+                ]
+            },
+            'total': 0
+        })
+    )
+    response = client.get('/product-combinations/data.json')
+
+    expected_link = '/product-combinations/+/many_results/data.json'
+    assert response.mimetype == 'application/json'
+    assert expected_link in response.json['product-combinations']
